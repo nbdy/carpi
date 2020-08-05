33fc7c66feae381889ac1c938f712e255dd89686
smthnspcl <pascal@eberlein.io>
2020-08-06 00:28:34 +0200

    update

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 sudo apt install qt5-default qtpositioning5-dev qtmultimedia5-dev qttools5-dev cmake libqt5svg5-dev
 
 sudo apt install -y libcairo2-dev libagg-dev libpango1.0-dev freeglut3-dev libglm-dev qtlocation5-dev \
-                    qml-module-qtpositioning qml-module-qtlocation
+                    qml-module-qtpositioning qml-module-qtlocation qtquickcontrols2-5-dev
 
 cd /tmp/
 if [ ! -d libosmscout ]; then

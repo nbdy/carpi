75f0cbda0f73d6b1a525a617ffd047de5b326f60
Pascal Eberlein <pascal@eberlein.io>
2021-11-15 00:09:38 +0100

    actually installing raylib

--- scripts/raylib.sh
+++ scripts/raylib.sh
@@ -12,22 +12,25 @@ fi
 
 ./scripts/dependencies.sh
 
-cd ext
-if [ ! -d "raylib" ]; then
+cd /tmp
+if [ ! -f "/usr/local/include/raylib.h" ]; then
   wget https://github.com/raysan5/raylib/archive/refs/tags/4.0.0.tar.gz
   tar xf 4.0.0.tar.gz
   rm 4.0.0.tar.gz
   mv raylib-4.0.0 raylib
+  cd raylib
+  if [ -d "build" ]; then
+    rm -rf build
+  fi
+  mkdir build
+  cd build
+  if [ "$#" == "1" ]; then
+    cmake .. -DPLATFORM="$1" -DBUILD_EXAMPLES=OFF -DBUILD_SHARED_LIBS=ON
+  else
+    cmake .. -DBUILD_EXAMPLES=OFF -DBUILD_SHARED_LIBS=ON
+  fi
+  make -j$(nproc)
+  sudo make install
+  cd /tmp
+  rm -rf raylib
 fi
-cd raylib
-if [ -d "build" ]; then
-  rm -rf build
-fi
-mkdir build
-cd build
-if [ "$#" == "1" ]; then
-  cmake .. -DPLATFORM="$1" -DBUILD_EXAMPLES=OFF
-else
-  cmake .. -DBUILD_EXAMPLES=OFF
-fi
-make -j$(nproc)
